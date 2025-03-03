e2fff5020d0d79c5701e11183028060e27f049ea
jenyxjs <hah.dev.lab@gmail.com>
2025-03-03 13:48:47 +0200

    refactor: rename Assets

--- www/src/SpeakArea/SpeakArea.js
+++ www/src/SpeakArea/SpeakArea.js
@@ -2,8 +2,8 @@ import { Control } from '../../lib/jenyx/components/Control/Control.js';
 import { SpeechRecognition } from './SpeechRecognition.js';
 import { Textarea } from '../Input/Textarea.js';
 import { Button } from '../App/Button.js';
-import { MIC_SVG } from '../Assert/MIC_SVG.js';
-import { COPY_SVG } from '../Assert/COPY_SVG.js';
+import { MIC_SVG } from '../Assets/MIC_SVG.js';
+import { COPY_SVG } from '../Assets/COPY_SVG.js';
 
 export class SpeakArea extends Control {
     constructor(options) {
